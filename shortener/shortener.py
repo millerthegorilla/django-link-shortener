b16b32ed1e7305af2d34216ef4f58fb4b9832e89
James Stewart Miller <jamesstewartmiller@gmail.com>
2026-02-15 22:13:49 +0000

    Fix syntax error in exception handling

--- shortener/shortener.py
+++ shortener/shortener.py
@@ -41,13 +41,14 @@ def create(user, link):
 
     except (UrlProfile.DoesNotExist, TypeError) as e:
         # Use defaults from settings
-        if (isinstance(e, UrlProfile.DoesNotExist) or (isinstance(e, TypeError) and getattr(settings, 'ALLOW_ANONYMOUS_USER', False)):
+        if (isinstance(e, UrlProfile.DoesNotExist) or (isinstance(e, TypeError) and getattr(settings, 'ALLOW_ANONYMOUS_USER', False))):
             enabled = getattr(settings, 'SHORTENER_ENABLED', True)
             max_urls = getattr(settings, 'SHORTENER_MAX_URLS', -1)
             max_concurrent = getattr(settings, 'SHORTENER_MAX_CONCURRENT', -1)
             lifespan = getattr(settings, 'SHORTENER_LIFESPAN', -1)
             max_uses = getattr(settings, 'SHORTENER_MAX_USES', -1)
-        else raise TypeError('Anonymous User is not allowed - check settings')
+        else:
+            raise TypeError('Anonymous User is not allowed - check settings')
 
     # Ensure User is allowed to create
     if not enabled:
